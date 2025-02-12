06d87ff886125cce3ecf8253772dcc6d504f5eb9
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2025-02-12 09:56:25 +0500

    update

---
+++ day_2/app.js
@@ -0,0 +1,31 @@
+// DESTRUCTURING 
+
+
+// let a = 1
+// let b = 2
+
+// let helper = a
+// a = b
+// b = helper
+
+// [a, b] = [b, a]
+
+// console.log("First way: ", a, b)
+// console.log("Second way: ", a, b) 
+
+
+
+
+
+// let arr = ["a" , "b" , "c" , "d" , "e" , "f"];   
+// let [first, second, ...rest] = arr;
+// console.log("First element: ", first);
+// console.log("Second element: ", second);
+// console.log("Rest of the array: ", rest);
+
+
+
+
+// let arr = ["a", "b", "c", "d", "e", "f"];
+// [arr[0], arr[5]] = [arr[5], arr[0]]
+// console.log("Reversed array: ", arr);
