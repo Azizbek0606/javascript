6a284f60852d80ee3db046c9a012d4130b3e5023
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2025-02-28 10:38:43 +0500

    update

--- channel-bot/commands/update.js
+++ channel-bot/commands/update.js
@@ -6,7 +6,7 @@ let allowedUpdates = ["name", "category_id", "description"];
 
 function updateUser(bot, msg) {
     const chatId = msg.chat.id;
-    const text = msg.text.trim();
+    let text = msg.text.trim();
 
     if (!userSteps[chatId]) {
         userSteps[chatId] = { step: 1, data: {} };
@@ -41,6 +41,20 @@ function updateUser(bot, msg) {
         if (allowedUpdates.includes(text)) {
             userData.choice = text;
             userData.step = 3;
+            if (userData.choice === "category_id") {
+                const categories = getCategories().map(cat => {
+                    const parts = cat.split(" - ");
+                    return { id: parseInt(parts[0], 10), name: parts[1] };
+                });
+
+                return bot.sendMessage(chatId, `Choose a category for this file:`, {
+                    reply_markup: {
+                        keyboard: categories.map(cat => [`${cat.id} - ${cat.name}`]),
+                        resize_keyboard: true,
+                        one_time_keyboard: true
+                    }
+                });
+            }
 
             return bot.sendMessage(chatId, `✍️ Send a new value for: ${userData.choice}`, {
                 reply_markup: {
@@ -58,11 +72,17 @@ function updateUser(bot, msg) {
             const categories = getCategories().map(cat => parseInt(cat.split(" - ")[0], 10));
 
             if (userData.choice === "category_id") {
-                const categoryId = parseInt(text, 10);
-                if (!categories.includes(categoryId)) {
-                    return bot.sendMessage(chatId, `⚠️ Invalid category ID! Please enter a valid ID between ${Math.min(...categories)} and ${Math.max(...categories)}.`);
+                const categoryIds = getCategories().map(cat => parseInt(cat.split(" - ")[0], 10));
+
+                const categoryId = parseInt(text.split(" - ")[0], 10);
+
+                if (!categoryIds.includes(categoryId)) {
+                    return bot.sendMessage(chatId, `⚠️ Invalid category ID! Please enter a valid ID between ${Math.min(...categoryIds)} and ${Math.max(...categoryIds)}.`);
                 }
+                text = categoryId
             }
+
+            
             if(userData.choice === "name"){
                 if(!checkText(text)){
                     return bot.sendMessage(chatId, "⚠️ Invalid file name! Please enter a valid name.");
