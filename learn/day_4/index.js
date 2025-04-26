475d729e73d4e703eb0d97858b7c4479f5bda24e
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2025-04-26 11:12:00 +0500

    update

--- learn/day_4/index.js
+++ learn/day_4/index.js
@@ -1,9 +1,10 @@
-let dict = { "lorem": "shoptoli" };
-for (let i = 0; i < 15; i++) {
-    dict["lorem" + i] = "shoptoli" + i;
-}
-// how to get key and value in loop from dict
-for (let key in dict) {
-    console.log(key, dict[key]);
-}
+// let dict = { "lorem": "shoptoli" };
+// for (let i = 0; i < 15; i++) {
+//     dict["lorem" + i] = "shoptoli" + i;
+// }
+// // how to get key and value in loop from dict
+// for (let key in dict) {
+//     console.log(key, dict[key]);
+// }
 
+console.log("lorem");
